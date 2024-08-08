c9c450e6d27c5b3a8dbdcbf5bd44a9b699010795
Mrdorawni <168426446+Mrdorawni@users.noreply.github.com>
2024-08-08 23:22:28 +0900

    Delete source directory

--- source/js/menu.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('menu-open-btn').addEventListener("click",()=>{
-        document.getElementById('menu').classList.add("display");
-});
-document.getElementById('menu-close').addEventListener("click",()=>{
-        document.getElementById('menu').classList.remove("display");
-});

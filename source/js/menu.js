49754e46c1a702d4ac503d13514bfc7461eaa5e6
Mrdorawni <168426446+Mrdorawni@users.noreply.github.com>
2024-08-08 23:27:52 +0900

    Add files via upload

---
+++ source/js/menu.js
@@ -0,0 +1,6 @@
+document.getElementById('menu-open-btn').addEventListener("click",()=>{
+        document.getElementById('menu').classList.add("display");
+});
+document.getElementById('menu-close').addEventListener("click",()=>{
+        document.getElementById('menu').classList.remove("display");
+});
